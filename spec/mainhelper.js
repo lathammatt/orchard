8b8a8a780b0f5ce39ce6145f0367b00a645a99bd
Matt Latham <latham75@gmail.com>
2016-07-01 15:36:59 -0500

    working main.js, some spec

---
+++ spec/mainhelper.js
@@ -0,0 +1,33 @@
+describe ("tree functions", function (){
+
+	it ("should have a plant function", function(){
+		expect(Plant).toBeDefined();
+		expect(this.height).toBeDefined;
+	});
+
+	it ("should have a tree function", function(){
+		expect(Tree).toBeDefined();
+	});	
+
+	it ("should have a increase height function", function(){
+		expect(increaseHeight).toBeDefined();
+		spyOn(Plant.protoype)
+	});	
+
+	it ("should have a decrease height function", function(){
+		expect(decreaseHeight).toBeDefined();
+	});	
+
+	it ("should have a grow function", function(){
+		expect(grow).toBeDefined();
+	});	
+
+	it ("should have a dtrim function", function(){
+		expect(trim).toBeDefined();
+	});
+
+
+
+
+
+})
